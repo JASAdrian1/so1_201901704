67cd30e23e35d29aaa1b10f2fe000034289847b9
Adrian <3022643370101@ingenieria.usac.edu.gt>
2023-02-16 12:04:51 -0600

    Se agregaron todos los reportes

--- Practica1/bash/script.sh
+++ Practica1/bash/script.sh
@@ -1,28 +1,33 @@
+#!/bin/bash
+url="logs.txt"
+date=$(date +'%d')
 echo "------------------REPORTE-----------------------" 
 echo -n "Cantidad de logs registrados: "
-cat logs.txt | wc -l
+cat $url | wc -l
 echo -n "Cantidad de operaciones resultadas en error: "
-cat logs.txt | grep e | wc -l
+cat $url | grep e | wc -l
 echo "Cantidad de operaciones:"
 echo -n "       Sumas:            "
-cat logs.txt | grep + | wc -l
+cat $url | grep + | wc -l
 echo -n "       Restas:           "   
-cat logs.txt | grep - | wc -l
+cat $url | grep - | wc -l
 echo -n "       Divsiones:        "
-cat logs.txt | grep / | wc -l
+cat $url | grep / | wc -l
 echo -n "       Multiplicaciones: "
-cat logs.txt | grep x | wc -l
+cat $url | grep x | wc -l
 echo "Logs de operaciones de hoy"
 while read line; do
-echo -n "Fecha: "
-printf $line | cut -d "," -f 1
-echo -n "   Valor1: "
-echo $line | cut -d "," -f 2
-echo -n "   Valor2: "
-echo $line | cut -d "," -f 3
-echo -n "   Operador: "
-echo $line | cut -d "," -f 5
-echo -n "   Resultado: "
-echo $line | cut -d "," -f 4
-done < "logs.txt"
+    if [[ $(echo "$line" | cut -d "," -f 1) == "$date" ]]; then
+        echo -n "Fecha: "
+        printf $line | cut -d "," -f 1
+        echo -n "   Valor1: "
+        echo $line | cut -d "," -f 2
+        echo -n "   Valor2: "
+        echo $line | cut -d "," -f 3
+        echo -n "   Operador: "
+        echo $line | cut -d "," -f 5
+        echo -n "   Resultado: "
+        echo $line | cut -d "," -f 4
+    fi
+done < $url
 echo "------------------------------------------------"
