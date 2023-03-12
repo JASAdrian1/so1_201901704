3bb8cf653aeb5c9f42b5c17577371ba3f6ba1a08
Adrian <3022643370101@ingenieria.usac.edu.gt>
2023-03-12 01:33:22 -0600

    Add dependecias go

--- Practica2/Backend/main.go
+++ Practica2/Backend/main.go
@@ -2,20 +2,61 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"os/exec"
+
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	cmd := exec.Command("sh", "-c", "/proc/cpu_201901704")
-	fmt.Println(cmd)
+	modulo_cpu := leer_modulo_cpu()
+	fmt.Println(modulo_cpu)
 }
 
 func leer_modulo_cpu() string {
-	cmd := exec.Command("sh", "-c", "/proc/cpu_201901704")
+	cmd := exec.Command("sh", "-c", "cat /proc/cpu_201901704")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println(err)
 	}
 	output := string(out[:])
 	return output
+}
+
+func insertarProcesos(procesos string) {
+	//Se establece la conexion con la base de datos
+	db, err := sql.Open("mysql", "root:0000@tcp(db-prac1:3306)/sopes1_p2")
+	defer db.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var version string
+
+	err2 := db.QueryRow("SELECT VERSION()").Scan(&version)
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	fmt.Println(version)
+
+}
+
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
 }
